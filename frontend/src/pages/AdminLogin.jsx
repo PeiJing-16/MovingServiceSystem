23f43e7aaea5871ea251c7d126d1663070876526
PeiJing-16 <ngpeijing8@gmail.com>
2026-04-02 05:13:40 +0000

    admin login

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -27,7 +27,6 @@ const AdminLogin = () => {
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center p-4"
-      style={{ backgroundImage: 'url("/background.png")' }}
     >
       <div className="w-full max-w-md bg-blue-100 p-8 rounded-xl shadow-xl">
         <h2 className="text-4xl font-bold text-center mb-6">Admin Login</h2>
